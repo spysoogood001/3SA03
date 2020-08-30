0163910d2888ae7d2f644d408fcb3054322dd0f4
spysoogood001 <spysoogood001@outlook.com>
2020-08-30 18:38:00 +0700

    part4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
+import Wordcard from './WordCard';
+
+const word = "Hello";
 function App() {
  return (
- <div>
- Hello World
- </div>
+  <div>
+    {
+      <Wordcard value={word}/>
+    }
+  </div>
  );
 }
 export default App;
